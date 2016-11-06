63e8f3fff5059ea0f08dc3f1ab88c74b343150f9
VashchukInna <vashchuk.inna@gmail.com>
2016-11-06 22:57:02 +0200

    4 task

--- session3/session3.js
+++ session3/session3.js
@@ -20,7 +20,9 @@ function compareObjects(firstObject, secondObject, compare) {
 }
 console.log(compareObjects(firstObject, secondObject, 'age'));
 
-/*Создайте один объект с помощью литерала, второй через конструктор.*/
+/*Создайте один объект с помощью литерала,
+ второй через конструктор.
+ */
 var objectLiteral = {
     name: "Anna",
     age: 20
@@ -32,8 +34,56 @@ function ObjectConstructor(name, last_name, age) {
     this.age = age;
 }
 
-var ObjectConstructor = new ObjectConstructor('Barbara', 'Allison', 30);
+var ObjectConstructor = new ObjectConstructor('Barbara', 'Ellison', 30);
+console.log(ObjectConstructor);
 
-/*Вызовите написанную функцию и передайте два созданных объекта и свойство для сравнения*/
+/*Вызовите написанную функцию и передайте два созданных объекта
+ и свойство для сравнения
+ */
 console.log(compareObjects(firstObject, secondObject, 'name'));
+
+
+//Поиск любимой песни
+/*Создайте коллекцию из 5 музыкальных песен, где каждая песня содержит следующую информацию:
+ played - количество раз песня была проиграна (определить случайным образом),
+ name - имя песни
+ */
+var collectionOfSongs = [
+    {
+        played: Math.round(Math.random() * 20),
+        name: 'Coldplay - Adventure of a Lifetime'
+    },
+    {
+        played: Math.round(Math.random() * 20),
+        name: 'Moby – Flower'
+    },
+    {
+        played: Math.round(Math.random() * 20),
+        name: 'Duke Dumont – Ocean Drive'
+    },
+    {
+        played: Math.round(Math.random() * 20),
+        name: 'Daft Punk – One more time'
+    },
+    {
+        played: Math.round(Math.random() * 20),
+        name: 'George Ezra – Budapest'
+    }
+];
+/*Напишите функцию favoriteSong, которая принимает коллекцию из песен,
+ и возвращает следующую информацию: название песни, сколько раз песня была проиграна, индекс песни в коллекции.
+ Вызовите функцию favoriteSong и передайте ей созданную коллекцию
+ */
+function favoriteSong(collectionOfSongs) {
+    var getSong = collectionOfSongs.reduce(function (previousValue, currentValue) {
+        if (previousValue.played > currentValue.played) {
+            return previousValue;
+        }
+        else {
+            return currentValue;
+        }
+    });
+    console.log('Name of song: ' + getSong.name + ', Times played: ' + getSong.played + ' Song index: ' + collectionOfSongs.indexOf(getSong));
+};
+favoriteSong(collectionOfSongs);
 
